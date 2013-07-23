11dd3cba4e0ad5c98a5560a1e7346ac6ae131549
Niraj <niraj38re@gmail.com>
2013-07-23 19:25:45 +0530

    Update gold.py

--- gold.py
+++ gold.py
@@ -2,15 +2,14 @@ import re
 import urllib
 import string
 ur = urllib.urlopen('http://www.exchangerates.org.uk/commodities/live-gold-prices/XAU-EUR.html')
-#print ur.read()
+
 
 html = ur.read()
 
 str1 = re.search("value_XAUEUR",html,re.I)
 
 if str1 :
-    #print 'ya-----'
-    #print str1.group()
+    
     index_val =  html.index("value_XAUEUR")
     index_val_update = index_val + 14
     #print index_val_update
@@ -20,4 +19,4 @@ if str1 :
     
     
 else :
-    print 'no'
+    print 'no live value '
